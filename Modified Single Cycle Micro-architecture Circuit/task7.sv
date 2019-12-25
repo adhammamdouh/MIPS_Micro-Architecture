2cf73bb4f291e7a57f30e1d592270e6e689d9d82
Belal Hamdy <36121094+belalhamdy@users.noreply.github.com>
2019-12-25 22:15:45 +0200

    Added belal's task

---
+++ Modified Single Cycle Micro-architecture Circuit/task7.sv
@@ -0,0 +1,132 @@
+module mips(input logic clk, reset,
+	output logic [31:0] pc,
+	input logic [31:0] instr,
+	output logic memwrite,
+	output logic [31:0] aluout, writedata,
+	input logic [31:0] readdata,
+	output logic readWriteType,chooseExtend); //Added By Belal
+	
+	logic memtoreg, alusrc, regdst,
+	regwrite, jump, pcsrc, zero;
+	logic [2:0] alucontrol;
+	
+	controller c(instr[31:26], instr[5:0], zero,
+	memtoreg, memwrite, pcsrc,
+	alusrc, regdst, regwrite, jump,
+	alucontrol,
+	readWriteType,chooseExtend); //Added By Belal
+	
+	datapath dp(clk, reset, memtoreg, pcsrc,
+	alusrc, regdst, regwrite, jump,
+	alucontrol,
+	zero, pc, instr,
+	aluout, writedata, readdata,
+	chooseExtend); //Added By Belal
+	
+endmodule
+module controller(input logic [5:0] op, funct,
+	input logic zero,
+	output logic memtoreg, memwrite,
+	output logic pcsrc, alusrc,
+	output logic regdst, regwrite,
+	output logic jump,
+	output logic [2:0] alucontrol,
+	output logic readWriteType,chooseExtend); //Added By Belal
+	
+	logic [1:0] aluop;
+	logic branch;
+	maindec md(op, memtoreg, memwrite, branch,
+	alusrc, regdst, regwrite, jump, aluop,
+	readWriteType,chooseExtend); //Added By Belal
+	
+	aludec ad(funct, aluop, alucontrol);
+	assign pcsrc = branch & zero;
+endmodule
+
+module maindec(input logic [5:0] op,
+output logic memtoreg, memwrite,
+output logic branch, alusrc,
+output logic regdst, regwrite,
+output logic jump,
+output logic [1:0] aluop,
+output logic readWriteType,chooseExtend); //Added By Belal
+
+	logic [8:0] controls;
+	assign {regwrite, regdst, alusrc, branch, memwrite,
+	memtoreg, jump, aluop,readWriteType,chooseExtend} = controls; //Added By Belal
+	always_comb
+	case(op)
+	6'b000000: controls <= 9'b11000001000; // RTYPE
+	6'b100011: controls <= 9'b10100100000; // LW
+	6'b101011: controls <= 9'b00101000000; // SW
+	6'b000100: controls <= 9'b00010000100; // BEQ
+	6'b001000: controls <= 9'b10100000000; // ADDI
+	6'b000010: controls <= 9'b00000010000; // J
+	6'b100000: controls <= 9'b10100100011; // LB Added By Belal as LW but readWriteType,chooseExtend  1
+	default: controls <= 9'bxxxxxxxxx; // illegal op
+	endcase
+endmodule
+
+module aludec(input logic [5:0] funct,
+	input logic [1:0] aluop,
+	output logic [2:0] alucontrol);
+	always_comb
+	case(aluop)
+	2'b00: alucontrol <= 3'b010; // add (for lw/sw/addi)
+	2'b01: alucontrol <= 3'b110; // sub (for beq)
+	default: case(funct) // R-type instructions
+		6'b100000: alucontrol <= 3'b010; // add
+		6'b100010: alucontrol <= 3'b110; // sub
+		6'b100100: alucontrol <= 3'b000; // and
+		6'b100101: alucontrol <= 3'b001; // or
+		6'b101010: alucontrol <= 3'b111; // slt
+		default: alucontrol <= 3'bxxx; // ???
+		endcase
+	endcase
+endmodule
+
+module datapath(input logic clk, reset,
+input logic memtoreg, pcsrc,
+input logic alusrc, regdst,
+input logic regwrite, jump,
+input logic [2:0] alucontrol,
+output logic zero,
+output logic [31:0] pc,
+input logic [31:0] instr,
+output logic [31:0] aluout, writedata,
+input logic [31:0] readdata,
+input logic chooseExtend);//Added By Belal
+	
+	logic [4:0] writereg;
+	logic [31:0] pcnext, pcnextbr, pcplus4, pcbranch;
+	logic [31:0] signimm, signimmsh;
+	logic [31:0] srca, srcb;
+	logic [31:0] result;
+	// next PC logic
+	flopr #(32) pcreg(clk, reset, pcnext, pc);
+	adder pcadd1(pc, 32'b100, pcplus4);
+	sl2 immsh(signimm, signimmsh);
+	adder pcadd2(pcplus4, signimmsh, pcbranch);
+	mux2 #(32) pcbrmux(pcplus4, pcbranch, pcsrc, pcnextbr);
+	mux2 #(32) pcmux(pcnextbr, {pcplus4[31:28],
+	instr[25:0], 2'b00}, jump, pcnext);
+	// register file logic
+	regfile rf(clk, regwrite, instr[25:21], instr[20:16],
+	writereg, result, srca, writedata);
+	mux2 #(5) wrmux(instr[20:16], instr[15:11],
+	regdst, writereg);
+	
+	//Added by belal
+	logic [31:0] byteRead;
+	logic [31:0] readDataResult;
+	signext se(readdata[0:7], byteRead);
+	
+	mux2 #(32) readDataMUX(readdata,byteRead,chooseExtend,readDataResult)
+	mux2 #(32) resmux(aluout, readDataResult, memtoreg, result);
+	
+	
+	signext se(instr[15:0], signimm);
+	// ALU logic
+	mux2 #(32) srcbmux(writedata, signimm, alusrc, srcb);
+	alu alu(srca, srcb, alucontrol, aluout, zero);
+endmodule
--- Single Cycle Micro-architecture/SingleCycleMicArc.sv
+++ Single Cycle Micro-architecture/SingleCycleMicArc.sv
@@ -1,294 +1,104 @@
-module Main_Decoder
-(
-	input logic [5:0] opcode,
-	output logic memto_reg,
-	output logic mem_write,	
-	output logic branch,
-	output logic alu_src,
-	output logic reg_dst,
-	output logic reg_write,
-	output logic [1:0] alu_op,
-	output logic jump
-);
-
-always_comb
-begin
-	case(opcode)
-		6'b000000 : begin
-			reg_write = 1'b1; 
-			reg_dst = 1'b1; 
-			alu_src = 1'b0;
-			branch = 1'b0;
-			mem_write = 1'b0;
-			memto_reg = 1'b0;
-			alu_op = 2'b10; 
-			jump = 1'b0; 
-		end
-		6'b100011 : begin
-			reg_write = 1'b1;
-			reg_dst = 1'b0;
-			alu_src = 1'b1;
-			branch = 1'b0;
-			mem_write = 1'b0;
-			memto_reg = 1'b1;
-			alu_op = 2'b00; 
-			jump = 1'b0; 
-		end
-		6'b101011 : begin
-			reg_write = 1'b0; 
-			reg_dst = 1'bx; 
-			alu_src = 1'b1;
-			branch = 1'b0;
-			mem_write = 1'b1;
-			memto_reg = 1'bx;
-			alu_op = 2'b00; 
-			jump = 1'b0; 
-		end
-		6'b000100 : begin
-			reg_write = 1'b0; 
-			reg_dst = 1'bx; 
-			alu_src = 1'b0;
-			branch = 1'b1;
-			mem_write = 1'b0;
-			memto_reg = 1'bx;
-			alu_op = 2'b01; 
-			jump = 1'b0; 
-		end
-		6'b001000 : begin
-			reg_write = 1'b1; 
-			reg_dst = 1'b0; 
-			alu_src = 1'b1;
-			branch = 1'b0;
-			mem_write = 1'b0;
-			memto_reg = 1'b0;
-			alu_op = 2'b00; 
-			jump = 1'b0; 
-		end
-		6'b000010 : begin
-			reg_write = 1'b0; 
-			reg_dst = 1'bx; 
-			alu_src = 1'bx;
-			branch = 1'bx;
-			mem_write = 1'b0;
-			memto_reg = 1'bx;
-			alu_op = 2'bxx; 
-			jump = 1'b1; 
-		end
-		default : begin
-		end 
-	endcase
-end
+module mips(input logic clk, reset,
+	output logic [31:0] pc,
+	input logic [31:0] instr,
+	output logic memwrite,
+	output logic [31:0] aluout, writedata,
+	input logic [31:0] readdata);
+	logic memtoreg, alusrc, regdst,
+	regwrite, jump, pcsrc, zero;
+	logic [2:0] alucontrol;
+	controller c(instr[31:26], instr[5:0], zero,
+	memtoreg, memwrite, pcsrc,
+	alusrc, regdst, regwrite, jump,
+	alucontrol);
+	datapath dp(clk, reset, memtoreg, pcsrc,
+	alusrc, regdst, regwrite, jump,
+	alucontrol,
+	zero, pc, instr,
+	aluout, writedata, readdata);
 endmodule
-
-module ALU_Decoder
-(
-	input logic [5:0] funct,
-	input logic [1:0] alu_op,
-	output logic [2:0] alu_control
-);
-always_comb
-begin
-	if(alu_op === 2'b00) begin
-		alu_control = 3'b010;
-	end
-	if(alu_op === 2'b01) begin
-		alu_control = 3'b110;
-	end
-	if(alu_op === 2'b10) begin
-		case(funct)
-			6'b1000_00: begin
-				alu_control = 3'b010;
-			end
-			6'b1000_10: begin
-				alu_control = 3'b110;
-			end
-			6'b1001_00: begin
-				alu_control = 3'b000;
-			end
-			6'b1001_01: begin
-				alu_control = 3'b001;
-			end
-			6'b1010_10: begin
-				alu_control = 3'b111;
-			end
+module controller(input logic [5:0] op, funct,
+	input logic zero,
+	output logic memtoreg, memwrite,
+	output logic pcsrc, alusrc,
+	output logic regdst, regwrite,
+	output logic jump,
+	output logic [2:0] alucontrol);
+	logic [1:0] aluop;
+	logic branch;
+	maindec md(op, memtoreg, memwrite, branch,
+	alusrc, regdst, regwrite, jump, aluop);
+	aludec ad(funct, aluop, alucontrol);
+	assign pcsrc = branch & zero;
+endmodule
+module maindec(input logic [5:0] op,
+output logic memtoreg, memwrite,
+output logic branch, alusrc,
+output logic regdst, regwrite,
+output logic jump,
+output logic [1:0] aluop);
+	logic [8:0] controls;
+	assign {regwrite, regdst, alusrc, branch, memwrite,
+	memtoreg, jump, aluop} = controls;
+	always_comb
+	case(op)
+	6'b000000: controls <= 9'b110000010; // RTYPE
+	6'b100011: controls <= 9'b101001000; // LW
+	6'b101011: controls <= 9'b001010000; // SW
+	6'b000100: controls <= 9'b000100001; // BEQ
+	6'b001000: controls <= 9'b101000000; // ADDI
+	6'b000010: controls <= 9'b000000100; // J
+	default: controls <= 9'bxxxxxxxxx; // illegal op
+	endcase
+endmodule
+module aludec(input logic [5:0] funct,
+	input logic [1:0] aluop,
+	output logic [2:0] alucontrol);
+	always_comb
+	case(aluop)
+	2'b00: alucontrol <= 3'b010; // add (for lw/sw/addi)
+	2'b01: alucontrol <= 3'b110; // sub (for beq)
+	default: case(funct) // R-type instructions
+		6'b100000: alucontrol <= 3'b010; // add
+		6'b100010: alucontrol <= 3'b110; // sub
+		6'b100100: alucontrol <= 3'b000; // and
+		6'b100101: alucontrol <= 3'b001; // or
+		6'b101010: alucontrol <= 3'b111; // slt
+		default: alucontrol <= 3'bxxx; // ???
 		endcase
-	end
-end
-
-endmodule
-
-module Control_Unit
-(
-	input logic [5:0] opcode,
-	input logic [5:0] funct,
-	output logic memto_reg,
-	output logic mem_write,	
-	output logic branch,
-	output logic [2:0] alu_control,
-	output logic alu_src,
-	output logic reg_dst,
-	output logic reg_write,
-	output logic jump
-);
-
-logic [1:0] alu_op;
-
-
-Main_Decoder Main_D(opcode, memto_reg, mem_write, branch, alu_src, reg_dst, reg_write, alu_op, jump);
-ALU_Decoder ALU_D(funct, alu_op, alu_control);
-
-endmodule
-
-module ALU
-(
-	input logic [31:0] src_a,
-	input logic [31:0] src_b,
-	input logic [2:0] alu_control,
-	output logic zero,
-	output logic [31:0] alu_result
-);
-
-always_comb
-begin
-	case(alu_control)
-		3'b010 : 
-			alu_result = src_a + src_b;
-		3'b110 : 
-			alu_result = src_a - src_b;
-		3'b000 :
-			alu_result = src_a & src_b;
-		3'b001 :
-			alu_result = src_a | src_b;
-		3'b111 :
-			alu_result = src_a < src_b;
 	endcase
-	zero = (alu_result === 32'b0)? 1 : 0;
-end
-
 endmodule
-
-module Sign_Extend
-(
-	input logic [15:0] input16, 
-	output logic [31:0] output32
-);
-always_comb
-begin
-	output32[31] = input16[15];
-	output32[30] = input16[15];
-	output32[29] = input16[15];
-	output32[28] = input16[15];
-	output32[27] = input16[15];
-	output32[26] = input16[15];
-	output32[25] = input16[15];
-	output32[24] = input16[15];
-	output32[23] = input16[15];
-	output32[22] = input16[15];
-	output32[21] = input16[15];
-	output32[20] = input16[15];
-	output32[19] = input16[15];
-	output32[18] = input16[15];
-	output32[17] = input16[15];
-	output32[16] = input16[15];
-	output32[15] = input16[15];
-	output32[14] = input16[14];
-	output32[13] = input16[13];
-	output32[12] = input16[12];
-	output32[11] = input16[11];
-	output32[10] = input16[10];
-	output32[9] = input16[9];
-	output32[8] = input16[8];
-	output32[7] = input16[7];
-	output32[6] = input16[6];
-	output32[5] = input16[5];
-	output32[4] = input16[4];
-	output32[3] = input16[3];
-	output32[2] = input16[2];
-	output32[1] = input16[1];
-	output32[0] = input16[0];
-end
+module datapath(input logic clk, reset,
+input logic memtoreg, pcsrc,
+input logic alusrc, regdst,
+input logic regwrite, jump,
+input logic [2:0] alucontrol,
+output logic zero,
+output logic [31:0] pc,
+input logic [31:0] instr,
+output logic [31:0] aluout, writedata,
+input logic [31:0] readdata);
+	logic [4:0] writereg;
+	logic [31:0] pcnext, pcnextbr, pcplus4, pcbranch;
+	logic [31:0] signimm, signimmsh;
+	logic [31:0] srca, srcb;
+	logic [31:0] result;
+	// next PC logic
+	flopr #(32) pcreg(clk, reset, pcnext, pc);
+	adder pcadd1(pc, 32'b100, pcplus4);
+	sl2 immsh(signimm, signimmsh);
+	adder pcadd2(pcplus4, signimmsh, pcbranch);
+	mux2 #(32) pcbrmux(pcplus4, pcbranch, pcsrc, pcnextbr);
+	mux2 #(32) pcmux(pcnextbr, {pcplus4[31:28],
+	instr[25:0], 2'b00}, jump, pcnext);
+	// register file logic
+	regfile rf(clk, regwrite, instr[25:21], instr[20:16],
+	writereg, result, srca, writedata);
+	mux2 #(5) wrmux(instr[20:16], instr[15:11],
+	regdst, writereg);
+	mux2 #(32) resmux(aluout, readdata, memtoreg, result);
+	signext se(instr[15:0], signimm);
+	// ALU logic
+	mux2 #(32) srcbmux(writedata, signimm, alusrc, srcb);
+	alu alu(srca, srcb, alucontrol, aluout, zero);
 endmodule
-
-module adder
-(
-	input logic [31:0] input_a,
-	input logic [31:0] input_b,
-	output logic [31:0] a_output
-);
-
-always_comb
-begin
-	a_output = input_a + input_b;
-end
-
-endmodule
-
-module mux32
-(
-	input logic [31:0] input_0,
-	input logic [31:0] input_1,
-	input logic selector, 
-	output logic [31:0] m_output
-);
-
-always_comb
-begin
-	case(selector)
-		1'b0: m_output = input_0;
-		1'b1: m_output = input_1;
-	endcase
-end
-endmodule 
-
-module mux5
-(
-	input logic [4:0] input_0,
-	input logic [4:0] input_1,
-	input logic selector, 
-	output logic [4:0] m_output
-);
-
-always_comb
-begin
-	case(selector)
-		1'b0: m_output = input_0;
-		1'b1: m_output = input_1;
-	endcase
-end
-endmodule  
-
-module Register_File
-(
-	input logic [4:0] a1,
-	input logic [4:0] a2,
-	input logic [4:0] a3,
-	input logic [31:0] wd3,
-	input logic reg_write,
-	input logic clk,
-	output logic [31:0] rd1,
-	output logic [31:0] rd2
-);
-
-reg [31:0] registers;
-
-initial
-begin
-	registers[0] = 0;
-end
-
-always @(negedge clk)
-begin
-	rd1 = registers[a1];
-	rd2 = registers[a2];
-end
-
-always @(posedge clk)
-begin
-	if (reg_write)
-	begin
-		registers[a3] = wd3;
-		registers[0] = 0;
-	end
-end
-
-endmodule 
